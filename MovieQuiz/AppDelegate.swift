85caa946eb80f8af2f6aa0fcda6025c5fd207394
qudryash <qudryashovav@yandex.ru>
2023-11-05 17:36:23 +0700

    выполнена работа по 4 спринту: - верстка по макету; - реализация логики по макету

--- MovieQuiz/AppDelegate.swift
+++ MovieQuiz/AppDelegate.swift
@@ -6,7 +6,7 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        print("Hello World!")
+        sleep(3)
         return true
     }
 
