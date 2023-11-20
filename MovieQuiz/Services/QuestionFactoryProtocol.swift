35f8c7003530dbf4e098fdb1691b61cd5c458a8d
qudryash <qudryashovav@yandex.ru>
2023-11-20 19:56:14 +0700

    Спринт 5. Абстракция фабрики вопросов: - создаем протокол фабрики вопросов QuestionFactoryProtocol; - в QuestionFactory привязали класс QuestionFactory к протоколу QuestionFactoryProtocol; - в контроллере перепривязали свойство questionFactory тип QuestionFactoryProtocol

---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Kudryashov Andrey on 20.11.2023.
+//
+
+import Foundation
+
+protocol QuestionFactoryProtocol {
+    func requestNextQuestion() -> QuizQuestion?
+}
