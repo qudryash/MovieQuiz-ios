9e5f61b939c1ebe6e9264235e2d34ea1c7f8e45b
qudryash <qudryashovav@yandex.ru>
2023-12-17 18:53:39 +0700

    Спринт 7. Изменен NetworkClient: - добавлен отдельный протокол "protocol NetworkRouting" - добавлено обращение к протоколу в структуре "struct NetworkClient" - изменена логика кода ответа с сервера с "ИЛИ" на "И"

--- MovieQuiz/Services/NetworkClient.swift
+++ MovieQuiz/Services/NetworkClient.swift
@@ -7,8 +7,11 @@
 
 import Foundation
 
+protocol NetworkRoiting {
+    func fetch(url: URL, handler: @escaping (Result<Data, Error>) -> Void)
+}
 /// Структура, которая отвечает за загрузку данных по URL
-struct NetworkClient {
+struct NetworkClient: NetworkRoiting {
     
     private enum NetworkError: Error {
         case codeError
@@ -25,7 +28,7 @@ struct NetworkClient {
             }
             
             if let response = response as? HTTPURLResponse,
-               response.statusCode < 200 || response.statusCode >= 300 {
+               response.statusCode < 200 && response.statusCode >= 300 {
                 handler(.failure(NetworkError.codeError))
                 return
             }
