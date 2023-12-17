4444b5aabb948f64f85001f36ce3f278de25ffd8
qudryash <qudryashovav@yandex.ru>
2023-12-17 19:01:56 +0700

    Спринт 7. Внесены изменения в "MoviesLoader".

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -14,7 +14,11 @@ protocol MoviesLoading {
 struct MoviesLoader: MoviesLoading {
     
     // MARK: - NetworkClient
-    private let networkClient = NetworkClient()
+    private let networkClient: NetworkRoiting
+    
+    init (networkClient: NetworkRoiting = NetworkClient()) {
+        self.networkClient = networkClient
+    }
     
     // MARK: - URL
     private var mostPopularMoviesUrl: URL {
