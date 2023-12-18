5d2e4606f6e6aed4e4187b8cc340f62bc8123483
qudryash <qudryashovav@yandex.ru>
2023-12-19 01:30:15 +0700

      Спринт 7. - Копируем метод convert(...) из MovieQuizViewController в MovieQuizPresenter - Изменен импорт библиотеки на UIKit

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -5,4 +5,15 @@
 //  Created by Kudryashov Andrey on 19.12.2023.
 //
 
-import Foundation
+import UIKit
+
+final class MovieQuizPresenter {
+    //    Метод конвертации из "Список вопросов" QuizQuestion в "Вопрос показан" QuizStepViewModel
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        return QuizStepViewModel (
+            image: UIImage(data: model.image) ?? UIImage(),
+            question: model.text,
+            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
+    }
+}
+
